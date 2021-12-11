79379e4961af0b3b8642bf2f69fa08cb102ba87d
resa23 <laresarianti23@gmail.com>
2021-12-12 03:24:54 +0700

    Percobaan 2

---
+++ P6_2_1204053/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace P6_2_1204053
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbJudul = new System.Windows.Forms.Label();
+            this.dgProdi = new System.Windows.Forms.DataGridView();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgProdi)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbJudul
+            // 
+            this.lbJudul.AutoSize = true;
+            this.lbJudul.Font = new System.Drawing.Font("Calibri", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbJudul.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lbJudul.Location = new System.Drawing.Point(263, 45);
+            this.lbJudul.Name = "lbJudul";
+            this.lbJudul.Size = new System.Drawing.Size(336, 35);
+            this.lbJudul.TabIndex = 0;
+            this.lbJudul.Text = "FORM VIEW UPDATE PRODI";
+            this.lbJudul.Click += new System.EventHandler(this.lbJudul_Click);
+            // 
+            // dgProdi
+            // 
+            this.dgProdi.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgProdi.Location = new System.Drawing.Point(96, 143);
+            this.dgProdi.Name = "dgProdi";
+            this.dgProdi.RowHeadersWidth = 62;
+            this.dgProdi.RowTemplate.Height = 28;
+            this.dgProdi.Size = new System.Drawing.Size(558, 250);
+            this.dgProdi.TabIndex = 1;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Font = new System.Drawing.Font("Microsoft Tai Le", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRefresh.Location = new System.Drawing.Point(214, 438);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(119, 45);
+            this.btnRefresh.TabIndex = 2;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(408, 440);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(118, 43);
+            this.btnUpdate.TabIndex = 3;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 559);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.dgProdi);
+            this.Controls.Add(this.lbJudul);
+            this.Font = new System.Drawing.Font("Microsoft Tai Le", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Master Program Studi";
+            ((System.ComponentModel.ISupportInitialize)(this.dgProdi)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbJudul;
+        private System.Windows.Forms.DataGridView dgProdi;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
+
---
+++ P6_2_1204053/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace P6_2_1204053
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // deklarasi variabel dsProdi dengan tipe dataset
+        private DataSet dsProdi;
+        public DataSet CreateProdiDataSet()
+        {
+            DataSet myDataSet = new DataSet();
+
+            try
+            {
+                //string myConnectionString = "integrated security=true;data source=.;initial catalog=LAPTOP-PH1JF1U0\BELAJARIT\P6_1204053";
+
+                SqlConnection myConnection = new SqlConnection(@"Data Source=LAPTOP-PH1JF1U0\BELAJARIT; Initial Catalog=P6_1204053;Integrated Security=True");
+
+                SqlCommand myCommand = new SqlCommand();
+
+                myCommand.Connection = myConnection;
+
+                myCommand.CommandText = "SELECT * FROM msprodi";
+                myCommand.CommandType = CommandType.Text;
+
+                SqlDataAdapter myDataAdapter = new SqlDataAdapter();
+
+                myDataAdapter.SelectCommand = myCommand;
+                myDataAdapter.TableMappings.Add("Table", "Prodi");
+
+                myDataAdapter.Fill(myDataSet);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+            return myDataSet;
+        }
+
+        private void RefreshDataset()
+        {
+            dsProdi = CreateProdiDataSet();
+
+            dgProdi.DataSource = dsProdi.Tables["Prodi"];
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RefreshDataset();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            //string myConnectionstring = "integrated security=true;data source=.;initial catalog=LAPTOP-PH1JF1U0\BELAJARIT\P6_1204053";
+
+            SqlConnection myConnection = new SqlConnection(@"Data Source=LAPTOP-PH1JF1U0\BELAJARIT; Initial Catalog=P6_1204053;Integrated Security=True");
+
+            myConnection.Open();
+
+            SqlDataAdapter myAdapter = new SqlDataAdapter("select * from msprodi", myConnection);
+            SqlCommandBuilder myCmdBuilder = new SqlCommandBuilder(myAdapter);
+
+            myAdapter.InsertCommand = myCmdBuilder.GetInsertCommand();
+            myAdapter.UpdateCommand = myCmdBuilder.GetUpdateCommand();
+            myAdapter.DeleteCommand = myCmdBuilder.GetDeleteCommand();
+
+            SqlTransaction myTransaction;
+
+            myTransaction = myConnection.BeginTransaction();
+            myAdapter.DeleteCommand.Transaction = myTransaction;
+            myAdapter.UpdateCommand.Transaction = myTransaction;
+            myAdapter.InsertCommand.Transaction = myTransaction;
+
+            try
+            {
+                int rowsUpdated = myAdapter.Update(dsProdi, "Prodi");
+
+                myTransaction.Commit();
+
+                MessageBox.Show(rowsUpdated.ToString() + " baris diperbarui", "informasi",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                RefreshDataset();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unable to update: " + ex.Message);
+                myTransaction.Rollback();
+            }
+          MessageBox.Show(myAdapter.InsertCommand.CommandText);
+          MessageBox.Show(myAdapter.UpdateCommand.CommandText);
+          MessageBox.Show(myAdapter.DeleteCommand.CommandText);
+        }
+
+        private void lbJudul_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
